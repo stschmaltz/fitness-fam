dc593732906cc0710918ab7b5121f26d81866212
shane schmaltz <stschmaltz@gmail.com>
2022-12-16 09:57:33 -0700

    add development warning

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,24 @@
-import { Container } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  CloseButton,
+  Container,
+} from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useLocalStorage } from '../hooks/use-local-storage.hook';
 import styles from '../styles/layout.module.css';
+import { theme } from '../styles/theme';
 
 export const siteTitle = 'Fitness Fam';
 
 export default function Layout({ children, home }) {
+  const [isVisible, setIsVisible] = useLocalStorage('alert', true);
+
   return (
     <Container mt={10}>
       <Head>
@@ -22,6 +34,26 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
+        {isVisible && (
+          <Alert colorScheme="brandPrimary" status="warning" pos="absolute">
+            <AlertIcon />
+            <Box color={theme.colors.brandPrimary['900']}>
+              <AlertTitle>Warning!</AlertTitle>
+              <AlertDescription>
+                This application is actively under development. Features/Visuals
+                are not complete and may break or change suddenly. Use with
+                caution.
+              </AlertDescription>
+            </Box>
+            <CloseButton
+              alignSelf="flex-start"
+              position="relative"
+              right={-1}
+              top={-1}
+              onClick={() => setIsVisible(false)}
+            />
+          </Alert>
+        )}
         {home && (
           <>
             <Image
