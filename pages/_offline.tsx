ab3f79f5425f45b904b35b881dfd0739d3f05225
shane schmaltz <shane.schmaltz@neofinancial.com>
2022-12-20 11:07:29 -0700

    fix offline

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,14 +1,12 @@
 import { Box, Text } from '@chakra-ui/react';
 
-export default class Offline extends Document {
-  render() {
-    return (
-      <Box>
-        <Text>
-          Offline Functionality Coming Soon. For now, please connect to the
-          internet to use fitnessfam.
-        </Text>
-      </Box>
-    );
-  }
+export default function Offline() {
+  return (
+    <Box>
+      <Text>
+        Offline Functionality Coming Soon. For now, please connect to the
+        internet to use fitnessfam.
+      </Text>
+    </Box>
+  );
 }
