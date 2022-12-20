41e0bf09492018d3b1a733cdaa3ecbefc5346568
shane schmaltz <stschmaltz@gmail.com>
2022-12-19 19:43:57 -0700

    fix thumbnail image maybe?

--- components/layout.tsx
+++ components/layout.tsx
@@ -31,12 +31,7 @@ export default function Layout(input: {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="App to help organize exercises" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={`./profile.png`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
