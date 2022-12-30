2c05a5e09286234fb3857e78e171024a4c96263a
shane schmaltz <stschmaltz@gmail.com>
2022-12-29 20:47:52 -0700

    use the local exercises instead of hitting the graphql endpoint to generate static exercise pages

--- pages/exercises/[id].tsx
+++ pages/exercises/[id].tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head';
 import { Text } from '@chakra-ui/react';
 
-import { asyncFetch } from '../../data/graphql/graphql-fetcher';
+// import { asyncFetch } from '../../data/graphql/graphql-fetcher';
 import Layout from '../../components/layout';
 import BasicExerciseInfo from '../../components/BasicExerciseInfo';
-import { fullExercise } from '../../data/graphql/snippets/exercise';
+// import { fullExercise } from '../../data/graphql/snippets/exercise';
 import { ExerciseObject } from '../../types/exercise';
+import { appContainer } from '../../container/inversify.config';
+import { ExerciseProviderInterface } from '../../providers/exercise.provider/exercise.provider.interface';
+import { TYPES } from '../../container/types';
 
 export default function Exercise(props: { exercise?: ExerciseObject }) {
   const { exercise } = props;
@@ -25,8 +28,12 @@ export default function Exercise(props: { exercise?: ExerciseObject }) {
 }
 
 export async function getStaticPaths() {
-  const exerciseIds = await asyncFetch(`{ exercises { id } }`);
-  const paths = exerciseIds.exercises.map((exercise: ExerciseObject) => ({
+  const exerciseProvider = appContainer.get<ExerciseProviderInterface>(
+    TYPES.ExerciseProvider
+  );
+  const exercises = await exerciseProvider.getAllExercises();
+
+  const paths = exercises.map((exercise: ExerciseObject) => ({
     params: {
       id: exercise.id,
     },
@@ -42,9 +49,10 @@ export async function getStaticProps({
 }: {
   params: { id: string };
 }) {
-  const exercise = await asyncFetch(
-    `{ exercise(id:"${id}"){ ${fullExercise} } }`
+  const exerciseProvider = appContainer.get<ExerciseProviderInterface>(
+    TYPES.ExerciseProvider
   );
+  const exercise = await exerciseProvider.findExerciseById(id);
 
   return {
     props: exercise,
